cd9bf1a72361ccd1d2c53a1d9d6b57640e289df2
Faithlmy <lmengyy@126.com>
2018-04-05 16:13:47 +0800

    abstract class

---
+++ src/April/a05/TestAbstractProgression.java
@@ -0,0 +1,18 @@
+package April.a05;
+
+public class TestAbstractProgression extends AbstractProgression {
+	
+	public static void main(String[] args) {
+		
+		TestAbstractProgression tap = new TestAbstractProgression();
+
+		tap.nextValue();
+	}
+
+	@Override
+	protected void advance() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

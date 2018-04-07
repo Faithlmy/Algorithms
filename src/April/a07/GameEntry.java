40dda14a3bd7c9c127842df7af22886ad3931270
Faithlmy <lmengyy@126.com>
2018-04-07 21:54:58 +0800

    add game

---
+++ src/April/a07/GameEntry.java
@@ -0,0 +1,29 @@
+package April.a07;
+
+public class GameEntry {
+	
+	private String name;
+	private int score;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+	public GameEntry(String n, int s) {
+		name = n;
+		score = s;
+	}
+	
+	public String toString() {
+		return "("+ name + ", " + score + ")";
+	}
+}

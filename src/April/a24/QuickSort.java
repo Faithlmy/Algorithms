ea95918ac2fa06d519c341f59a53613c7a7d8fe0
Faithlmy <lmengyy@126.com>
2018-04-24 09:16:22 +0800

    modify Quick sort

--- src/April/a24/QuickSort.java
+++ src/April/a24/QuickSort.java
@@ -11,18 +11,12 @@ public class QuickSort {
 	public void QuickCode(int[] arr, int low, int high) {
 		if(low < high) {
 			int mid = QuickMid(low, high);
-			QuickCode(arr, 0, mid - 1 );
+			QuickCode(arr, low, mid - 1 );
 			QuickCode(arr, mid + 1, high);
 		}
 		
 	}
 
-	public void QuickPrint() {
-		for(int i = 0; i <this.arr.length; i++) {
-			System.out.print(this.arr[i] + ", ");
-		}
-		System.out.println();
-	}
 
 	private int QuickMid(int low, int high) {
 		int key = this.arr[low];
@@ -38,6 +32,13 @@ public class QuickSort {
 		}
 		this.arr[low] = key;
 		return low;
+	}
+	
+	public void QuickPrint() {
+		for(int i = 0; i <this.arr.length; i++) {
+			System.out.print(this.arr[i] + ", ");
+		}
+		System.out.println();
 	}
 
 }
--- src/April/a24/TestQuick.java
+++ src/April/a24/TestQuick.java
@@ -1,21 +1,32 @@
 package April.a24;
 
 import April.a21.RandomArray;
+import April.a23.BubbleSort;
 
 public class TestQuick {
 	
 	public static void main(String[] args) throws Exception {
 		RandomArray ra = new RandomArray();
-		int[] a = ra.ArrayRandom(100, 1000);
-		ra.ArrayPrint();
+		int[] a = ra.ArrayRandom(1000000, 10000000);
+//		long startTime2 = System.currentTimeMillis();
+//		ra.ArrayPrint();
+//		long endTime2 = System.currentTimeMillis();
+//		System.out.println(endTime2 - startTime2);
 		
 		QuickSort qs = new QuickSort(a);
-//		long startTime = System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
 		qs.QuickCode(a, 0, a.length - 1);
-//		long endTime = System.currentTimeMillis();
-		qs.QuickPrint();
-//		System.out.println(endTime - startTime);
+		long endTime = System.currentTimeMillis();
+		System.out.println(endTime - startTime);
+//		qs.QuickPrint();
 		
+		
+		BubbleSort bs = new BubbleSort(a);
+		long startTime1 = System.currentTimeMillis();
+		bs.bsCode();
+		long endTime1 = System.currentTimeMillis();
+		System.out.println(endTime1 - startTime1);
+//		bs.BubblePrint();
 		
 		
 		
